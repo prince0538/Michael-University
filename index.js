90ce6c22e59a0ba9b68a4fc154295d99b550474e
iyanu9491-netizen <iyanu9491@gmail.com>
2026-03-14 22:55:42 +0100

    just upadeted the server file

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const PORT = 4000
 
 const app = express();
-
-const Router = require('./router/facultyRoutes')
-const studenttableRouter = require("./router/studenttableRoutes")
 app.use(express.json());
 
+const Router = require('./router/facultyRoutes')
+const deptRouter = require('./router/departmentRoutes')
+const studentRouter = require('./router/studenttableRoutes')
+const staffRout = require('./router/staffRouter')
 app.use(Router)
-app.use(studenttableRouter )
+app.use(deptRouter)
+app.use(studentRouter)
 
 app.listen(PORT,() => {
     console.log(`Server is running on PORT:${PORT}`)
