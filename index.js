95716983b2fbcc062352cf4fb5104d52a0c23b25
iyanu9491-netizen <iyanu9491@gmail.com>
2026-03-14 21:37:57 +0100

    just finishing writing the studentTable controller and the rest file

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const PORT = 4000
 const app = express();
 
 const Router = require('./router/facultyRoutes')
+const studenttableRouter = require("./router/studenttableRoutes")
 app.use(express.json());
 
 app.use(Router)
+app.use(studenttableRouter )
 
 app.listen(PORT,() => {
     console.log(`Server is running on PORT:${PORT}`)
