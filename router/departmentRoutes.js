5abdacbcb5738459f03407331f1d9e30d641581d
iyanu9491-netizen <iyanu9491@gmail.com>
2026-03-16 12:38:39 +0100

    just updated the router file

--- router/departmentRoutes.js
+++ router/departmentRoutes.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router();
-const {addDep} = require('../controller/departmentController');
+const {addDep,findDep} = require('../controller/departmentController');
 
 Router.post('/departments/:facultyId', addDep)
+Router.get('/department',findDep )
 
 module.exports = Router
