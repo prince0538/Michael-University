a658bde0138fa17103cf4b7804d903b4b60a7800
iyanu9491-netizen <iyanu9491@gmail.com>
2026-03-16 11:06:16 +0100

    just updated my codes

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.use(express.json());
 const Router = require('./router/facultyRoutes')
 const deptRouter = require('./router/departmentRoutes')
 const staffRout = require('./router/staffRouter')
-app.use(Router)
+app. use(Router)
 app.use(deptRouter)
+app.use(staffRout)
 
 app.listen(PORT,() => {
     console.log(`Server is running on PORT:${PORT}`)
